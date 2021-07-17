54abec2926b9ac74d6c222b1118b374c0f976440
Felix Kong <felix.kong@uts.edu.au>
2021-07-17 20:11:44 +1000

    input is a directory now

--- parse-timeline.py
+++ parse-timeline.py
@@ -1,55 +1,60 @@
 #!/usr/bin/env python3
 import json
 import sys
+import os
 from datetime import datetime
 
 # parse args
-filename = sys.argv[1]
+dir_name = sys.argv[1]
 if sys.argv[2] == 'verbose' or sys.argv[2] == '--verbose':
     verbose = sys.argv[2]
 else:
     verbose = False
 
-if verbose:
-    print('Opening {}'.format(filename))
 
-with open(filename) as f:
-    # Load in Google Location History data.
-    data = json.load(f)
+for subdir, dirs, files in os.walk(dir_name):
+    for file in files:
+        filename = dir_name + '/' + file
+        with open(filename) as f:
+            # Load in Google Location History data.
+            data = json.load(f)
 
-fout = open('semantic_timeline.csv','a')
-fout.write('startTime,endTime,locationName,address,confidence\n')
-
-if verbose:
-    print('There are {} entries to look through'.format(len(data['timelineObjects'])))
-
-for entry in data['timelineObjects']:
-    try:
-        placeVisit = entry['placeVisit']
-    except:
         if verbose:
-            print('This entry has no "placeVisit" field.')
-        continue
+            print('Opening {}'.format(dir_name))
 
-    locationObj = placeVisit['location']
-    locationName = locationObj['name']
-    try:
-        address = locationObj['address'].replace('\n',' ')
-    except:
+        fout = open('semantic_timeline.csv','a')
+        fout.write('startTime,endTime,locationName,address,confidence\n')
+
         if verbose:
-            print('Address not found.')
-        address = ''
-        
-    confidence = locationObj['locationConfidence']
+            print('There are {} entries to look through'.format(len(data['timelineObjects'])))
 
-    durationObj = placeVisit['duration']
-    # already determines local time?
-    startTime = datetime.fromtimestamp(int(durationObj['startTimestampMs'])/1e3) # convert from ms to to seconds
-    endTime = datetime.fromtimestamp(int(durationObj['endTimestampMs'])/1e3)
+        for entry in data['timelineObjects']:
+            try:
+                placeVisit = entry['placeVisit']
+            except:
+                if verbose:
+                    # print('This entry has no "placeVisit" field.')
+                continue
 
-    fout.write('{},{},{},{},{}\n'.format(startTime,endTime,locationName,address,confidence))
-    if verbose:
-        print('Added entry: {} at {}'.format(startTime,locationObj['name']))
+            locationObj = placeVisit['location']
+            locationName = locationObj['name']
+            try:
+                address = locationObj['address'].replace('\n',' ')
+            except:
+                if verbose:
+                    print('Address not found.')
+                address = ''
+                
+            confidence = locationObj['locationConfidence']
+
+            durationObj = placeVisit['duration']
+            # already determines local time?
+            startTime = datetime.fromtimestamp(int(durationObj['startTimestampMs'])/1e3) # convert from ms to to seconds
+            endTime = datetime.fromtimestamp(int(durationObj['endTimestampMs'])/1e3)
+
+            fout.write('{},{},{},{},{}\n'.format(startTime,endTime,locationName,address,confidence))
+            if verbose:
+                print('Added entry: {} at {}'.format(startTime,locationObj['name']))
 
 if verbose:
     print('Done')
